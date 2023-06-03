d12f4fc687c5f5556fb9278ce97fc0b16a1dc0a1
ABojo <Alex.bojorquez2598@gmail.com>
2023-06-02 20:39:46 -0400

    created an object that holds references the css selectors that will be used in the javascript

---
+++ src/ts/utils/css.ts
@@ -0,0 +1,12 @@
+const css = {
+  billGroup: "#bill-group",
+  tipGroup: "#tip-group",
+  peopleGroup: "#people-group",
+  tipAmount: "#tip-amount",
+  totalAmount: "#total-amount",
+  resetButton: "#reset",
+  resetDisabled: "output__reset--disabled",
+  groupError: "form-group--error",
+};
+
+export default css;
